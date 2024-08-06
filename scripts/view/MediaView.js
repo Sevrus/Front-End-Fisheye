79b266adfd4b7ee53bb4cddd6224fca89f84d21d
Sevrus <book-n-troll@nordnet.fr>
2024-08-06 16:50:39 +0200

    Styled gallery and started styling the select element
    - Improved gallery styling
    - Began styling the select element

--- scripts/view/MediaView.js
+++ scripts/view/MediaView.js
@@ -4,17 +4,21 @@ class MediaView {
     }
 
     createMediaDOM() {
-        const { title, like } = this.model;
+        const { title, likes } = this.model;
         const mediaPath = this.model.getMediaPath();
 
-        const mediaArticle = document.createElement('article');
+        const mediaFigure = document.createElement('figure');
 
         const mediaElement = document.createElement(this.model.image ? 'img' : 'video');
+        mediaElement.className = 'media-element';
         mediaElement.setAttribute('src', mediaPath);
         mediaElement.setAttribute('alt', title);
         if (this.model.video) {
             mediaElement.setAttribute('controls', 'controls');
         }
+
+        const figcaptionElement = document.createElement('figcaption');
+        figcaptionElement.className = 'figcaption-element';
 
         const titleElement = document.createElement('h2');
         titleElement.textContent = title;
@@ -22,20 +26,22 @@ class MediaView {
         const likesElement = document.createElement('div');
         likesElement.className = 'likes';
 
+        const likesCount = document.createElement('span');
+        likesCount.textContent = `${likes} `;
+
         const heartIcon = document.createElement('i');
         heartIcon.className = 'fas fa-heart';
 
-        const likesCount = document.createElement('span');
-        likesCount.textContent = ` ${like}`;
-
-        likesElement.appendChild(heartIcon);
         likesElement.appendChild(likesCount);
+        likesElement.appendChild(heartIcon);
 
-        mediaArticle.appendChild(mediaElement);
-        mediaArticle.appendChild(titleElement);
-        mediaArticle.appendChild(likesElement);
+        figcaptionElement.appendChild(titleElement);
+        figcaptionElement.appendChild(likesElement);
 
-        return mediaArticle;
+        mediaFigure.appendChild(mediaElement);
+        mediaFigure.appendChild(figcaptionElement);
+
+        return mediaFigure;
     }
 }
 
